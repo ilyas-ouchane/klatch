bedf988bf3990d3dba69dd6d9332fced7435d57f
Reda-Eladnani <eladnani123.are@gmail.com>
2020-05-12 02:17:36 +0000

    preferences added

--- app.py
+++ app.py
@@ -2,9 +2,7 @@ from flask import Flask , render_template, request, session, logging, url_for, r
 from flask_pymongo import PyMongo
 from passlib.hash import sha256_crypt
 
-# engine = create_engine("mysql+pymysql://root:@localhost/klatch")
 
-# db = scoped_session(sessionmaker(bind=engine))
 app = Flask(__name__)
 
 app.config['MONGO_DBNAME'] = 'klatch'
@@ -12,19 +10,51 @@ app.config['MONGO_URI'] = 'mongodb://localhost:27017/klatch'
 
 mongo = PyMongo(app)
 
-#page d'accueil et register
+#initial and register page
 @app.route("/", methods=["GET", "POST"])
 def index():
     
     if request.method == "POST":
         users = mongo.db.user
+        preferences = mongo.db.preferences
+
         nom = request.form.get("nom")
         prenom = request.form.get("prenom")
         password = request.form.get("password")
         email = request.form.get("email")
         secure_password = sha256_crypt.encrypt(str(password))
+        age = request.form.get("age")
+
+        #preferences :
+        musicG = request.form.get("musicG")
+        hiphop = request.form.get("hiphop")
+        rap = request.form.get("rap")
+        raggae = request.form.get("raggae")
+        swing = request.form.get("swing")
+        jazz = request.form.get("jazz")
+        moviesG = request.form.get("moviesG")
+        comedy = request.form.get("comedy")
+        romantic = request.form.get("romantic")
+        scif = request.form.get("scif")
+        war = request.form.get("war")
+        fantazy = request.form.get("fantazy")
+        documentary = request.form.get("documentary")
+        western = request.form.get("western")
+        history = request.form.get("history")
+        psychology = request.form.get("psychology")
+        politics = request.form.get("politics")
+        reading = request.form.get("reading")
+        languages = request.form.get("languages")
+        musicalIns = request.form.get("musicalIns")
+        writing = request.form.get("writing")
+        sport = request.form.get("sport")
+        celebrities = request.form.get("celebrities")
+        science = request.form.get("science")
+        theatre = request.form.get("theatre")
 
         users.insert({"nomuser":nom, "prenomuser":prenom, "emailuser":email, "mdp":secure_password})
+        user = users.find_one({"emailuser":email})
+        preferences.insert({"id":user["_id"], "Music":musicG, "Rap":rap, "Raggae":raggae, "Swing":swing, "Jazz":jazz, "Movies":moviesG, "Comedy":comedy, "Romantic":romantic, "Sci-fi":scif, "War":war, "Fantazy/Fairy tales":fantazy, "Documentary":documentary, "Western":western, "History":history, "Psychology":psychology, "Politics":politics, "Reading":reading, "Languages":languages, "Musical instruments":musicalIns, "Writing":writing, "Sport":sport, "Celebrities":celebrities, "Science and technology":science, "Theatre":theatre, "Age":age})
 
         return redirect(url_for('login'))
     
