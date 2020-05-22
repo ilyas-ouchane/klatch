62ec45dc311f875ff297f0724d2259fcc1a39932
ilyas-ouchane <ouchane.ilyas@gmail.com>
2020-05-22 19:38:08 +0000

    dm commit

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from flask import Flask , render_template, request, session, logging, url_for, redirect,flash
 from flask_pymongo import PyMongo
 from passlib.hash import sha256_crypt
+from sklearn import svm
+from sklearn import metrics
+import pickle
+import pandas as pd
+import numpy as np
 
 app = Flask(__name__)
 
@@ -19,12 +24,34 @@ def index():
     crPreferences = mongo.db.chatroompreferences
     nomsChatrooms = chatrooms.find({}, {'nomchatroom': 1, 'description':1})
     preferencesChatrooms = crPreferences.find({}, {"Music":1, "Rap":1, "Raggae":1, "Swing":1, "Jazz":1, "Movies":1, "Comedy":1, "Romantic":1, "Sci-fi":1, "War":1, "Fantazy/Fairy tales":1, "Documentary":1, "Western":1, "History":1, "Psychology":1, "Politics":1, "Reading":1, "Languages":1, "Musical instruments":1, "Writing":1, "Sport":1, "Celebrities":1, "Science and technology":1, "Theatre":1})
-    return render_template('index.html', nomsChatrooms=nomsChatrooms, preferencesChatrooms=preferencesChatrooms)
+    preferencesTab = ["Music", "Rap", "Raggae", "Swing", "Jazz", "Movies", "Comedy", "Romantic", "Sci-fi", "War", "Fantazy/Fairy tales", "Documentary", "Western", "History", "Psychology", "Politics", "Reading", "Languages", "Musical instruments", "Writing", "Sport", "Celebrities", "Science and technology", "Theatre"]
+    return render_template('index.html', nomsChatrooms=nomsChatrooms, preferencesChatrooms=preferencesChatrooms, preferencesTab=preferencesTab)
 #profile
 @app.route("/profile")
 def profile():
     return render_template('profile.html')
 
+#admin
+@app.route("/admin")
+def admin():
+    chatrooms = mongo.db.chatroom
+    crPreferences = mongo.db.chatroompreferences
+    nomsChatrooms = chatrooms.find({}, {'nomchatroom': 1, 'description':1})
+    preferencesChatrooms = crPreferences.find({}, {"Music":1, "Rap":1, "Raggae":1, "Swing":1, "Jazz":1, "Movies":1, "Comedy":1, "Romantic":1, "Sci-fi":1, "War":1, "Fantazy/Fairy tales":1, "Documentary":1, "Western":1, "History":1, "Psychology":1, "Politics":1, "Reading":1, "Languages":1, "Musical instruments":1, "Writing":1, "Sport":1, "Celebrities":1, "Science and technology":1, "Theatre":1})
+    preferencesTab = ["Music", "Rap", "Raggae", "Swing", "Jazz", "Movies", "Comedy", "Romantic", "Sci-fi", "War", "Fantazy/Fairy tales", "Documentary", "Western", "History", "Psychology", "Politics", "Reading", "Languages", "Musical instruments", "Writing", "Sport", "Celebrities", "Science and technology", "Theatre"]
+    return render_template('admin.html', nomsChatrooms=nomsChatrooms, preferencesChatrooms=preferencesChatrooms, preferencesTab=preferencesTab)
+
+#delete chatroom
+@app.route("/deletecr", methods=["GET", "POST"])
+def deletecr():
+    if request.method == "POST":
+        chatrooms = mongo.db.chatroom
+        nomcr = request.form.get("nomcr")
+        chatrooms.remove({'nomchatroom': nomcr})
+
+        return render_template('admin.html')
+
+    return render_template('admin.html')
 #register page
 @app.route("/register", methods=["GET", "POST"])
 def register():
@@ -38,6 +65,7 @@ def register():
         email = request.form.get("email")
         secure_password = sha256_crypt.encrypt(str(password))
         age = request.form.get("age")
+        genre = request.form.get("genre")
 
         #preferences :
         musicG = request.form.get("musicG")
@@ -68,11 +96,41 @@ def register():
 
         users.insert({"nomuser":nom, "prenomuser":prenom, "emailuser":email, "mdp":secure_password})
         user = users.find_one({"emailuser":email})
-        preferences.insert({"id":user["_id"], "Music":musicG, "Rap":rap, "Raggae":raggae, "Swing":swing, "Jazz":jazz, "Movies":moviesG, "Comedy":comedy, "Romantic":romantic, "Sci-fi":scif, "War":war, "Fantazy/Fairy tales":fantazy, "Documentary":documentary, "Western":western, "History":history, "Psychology":psychology, "Politics":politics, "Reading":reading, "Languages":languages, "Musical instruments":musicalIns, "Writing":writing, "Sport":sport, "Celebrities":celebrities, "Science and technology":science, "Theatre":theatre, "Age":age})
+        preferences.insert({"id":user["_id"], "Music":musicG, "Rap":rap, "Raggae":raggae, "Swing":swing, "Jazz":jazz, "Movies":moviesG, "Comedy":comedy, "Romantic":romantic, "Sci-fi":scif, "War":war, "Fantazy/Fairy tales":fantazy, "Documentary":documentary, 
+        "Western":western, "History":history, "Psychology":psychology, "Politics":politics, "Reading":reading, "Languages":languages, "Musical instruments":musicalIns, "Writing":writing, "Sport":sport, "Celebrities":celebrities, "Science and technology":science, 
+        "Theatre":theatre, "Age":age, "genre": genre})
 
-        return redirect(url_for('login'))
+        ######################## Testing the model##########################################
+        list=[musicG,hiphop,rap,raggae,swing,jazz,moviesG,comedy,romantic,scif,war,fantazy,documentary,western,history,psychology,
+        politics,reading,languages,musicalIns,writing,sport,celebrities,science,theatre,age,genre]
+
+        def test_model(list):
+            #     pickle.dump(clf, open(fn, 'wb'))
+            filename = 'final_model_klatch.sav'
+            loaded_model = pickle.load(open(filename, 'rb'))
+            A = np.array([list])
+            B = A.reshape(1,-1)
+            result = loaded_model.predict(B)
+            return(result[0])
+
+        chatroom_recommended_id = test_model(list)
+        ###########################################################
+        ## Now we need to find a way to : 
+                 ## Display the recommended chat-room that has the ID of recommended_chatroom_id  
+                 ## in another page --> redirect to it 
+                 ## Register the user 
+        ###########################################################
+
+
+
+        return render_template("list.html", chatroom_recommended_id=chatroom_recommended_id)
     return render_template("register.html")
 
+#list
+
+@app.route("/list")
+def list():
+    return render_template("list.html")
 #register page
 @app.route("/create_chatroom", methods=["GET", "POST"])
 def create_chatroom():
